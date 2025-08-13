950eb2c74d396189b3b32deb54c9030bfdee9ed9
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-08-13 20:31:28 -0300

    Update: buscarEpisodioPorSerie()

--- src/main/java/br/com/screenmatch/main/Main.java
+++ src/main/java/br/com/screenmatch/main/Main.java
@@ -2,12 +2,15 @@ package br.com.screenmatch.main;
 
 import br.com.screenmatch.model.DadosSerie;
 import br.com.screenmatch.model.DadosTemporada;
+import br.com.screenmatch.model.Serie;
 import br.com.screenmatch.service.ConsumoApi;
 import br.com.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -83,6 +86,13 @@ public class Main {
     }
 
     private void listarSeriesBuscadas(){
-        dadosSeries.forEach(System.out::println);
+        List<Serie> series = new ArrayList<>();
+        series = dadosSeries.stream()
+                .map(s -> new Serie(s))
+                        .collect(Collectors.toList());
+
+        series.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
     }
 }
